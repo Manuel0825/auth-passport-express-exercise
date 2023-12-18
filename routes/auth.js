1a3a14f8a495528478634ebb435b77259e13dc52
Manuel Vallejo <manuelvallejo@Manuels-MacBook-Pro.local>
2023-12-18 21:01:13 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -27,7 +27,7 @@ router.post("/register", async (req, res) => {
 router.post(
   "/login",
   passport.authenticate("local", {
-    successRedirect: "/profile-page",
+    successRedirect: "/posts/profile-page",
     failureRedirect: "/login-page",
     failureFlash: true,
   })
